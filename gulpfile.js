2a48c9de4be0bc02362ff83bb5956de6dfb68746
Rachel Carden <Rachel.Carden@disney.com>
2017-12-05 21:49:54 -0800

    Cleaning up composer and gulp workflows

--- gulpfile.js
+++ gulpfile.js
@@ -1,88 +1,104 @@
-// Require all the things (that we need)
-var autoprefixer = require('gulp-autoprefixer');
-var gulp = require('gulp');
-var phpcs = require('gulp-phpcs');
-var rename = require('gulp-rename');
-var sass = require('gulp-sass');
-var sort = require('gulp-sort');
-var uglify = require('gulp-uglify');
-var watch = require('gulp-watch');
-var wp_pot = require('gulp-wp-pot');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const gulp = require('gulp');
+const mergeMediaQueries = require('gulp-merge-media-queries');
+const notify = require('gulp-notify');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const shell = require('gulp-shell');
+const sort = require('gulp-sort');
+const uglify = require('gulp-uglify');
+const wp_pot = require('gulp-wp-pot');
 
-// Define the source paths for each file type
-var src = {
-    scss: 'assets/scss/*.scss',
-    js: ['assets/js/*.js','!assets/js/*.min.js'],
-    php: ['**/*.php','!vendor/**','!node_modules/**']
+// Define the source paths for each file type.
+const src = {
+	js: ['assets/js/admin-post-schedule.js','assets/js/admin-post-speakers.js','assets/js/conf-schedule.js','assets/js/conf-schedule-single.js'],
+	php: ['**/*.php','!vendor/**','!node_modules/**'],
+	sass: ['assets/scss/**/*.scss']
 };
 
-// Define the destination paths for each file type
-var dest = {
-	scss: './assets/css',
-	js: './assets/js'
+// Define the destination paths for each file type.
+const dest = {
+	js: 'assets/js',
+	sass: 'assets/css',
+	translate: 'languages/conf-schedule.pot'
 };
 
-// I've got my eyes on you(r file changes)
-gulp.task('watch', function() {
-	gulp.watch(src.scss, ['sass']);
-	gulp.watch(src.js, ['js']);
-	gulp.watch(src.php, ['test','translate']);
-});
-
-// Sass is pretty awesome, right?
+// Take care of SASS.
 gulp.task('sass', function() {
-	return gulp.src(src.scss)
+	return gulp.src(src.sass)
 		.pipe(sass({
 			outputStyle: 'compressed'
-		})
-		.on('error', sass.logError))
+		}).on('error', sass.logError))
+		.pipe(mergeMediaQueries())
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions'],
 			cascade: false
 		}))
-		.pipe(rename({ suffix: '.min' }))
-		.pipe(gulp.dest(dest.scss));
+		.pipe(cleanCSS({
+			compatibility: 'ie8'
+		}))
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest(dest.sass))
+		.pipe(notify('Conference Schedule SASS compiled'));
 });
 
 // Minify the JS
 gulp.task('js', function() {
-    gulp.src(src.js)
-        .pipe(uglify({
-            mangle: false
-        }))
-        .pipe(rename({
+	gulp.src(src.js)
+		.pipe(uglify({
+			mangle: false
+		}))
+		.pipe(rename({
 			suffix: '.min'
 		}))
-        .pipe(gulp.dest(dest.js))
+		.pipe(gulp.dest(dest.js))
 });
 
-// Check our PHP
-gulp.task('php',function() {
-	gulp.src(src.php)
-		.pipe(phpcs({
-			bin: 'vendor/bin/phpcs',
-			standard: 'WordPress-Core'
+// "Sniff" our PHP.
+gulp.task('php', function() {
+	// TODO: Clean up. Want to run command and show notify for sniff errors.
+	return gulp.src('conference-schedule.php', {read: false})
+		.pipe(shell(['composer sniff'], {
+			ignoreErrors: true,
+			verbose: false
 		}))
-		.pipe(phpcs.reporter('log'));
+		.pipe(notify('Conference Schedule PHP sniffed'), {
+			onLast: true,
+			emitError: true
+		});
 });
 
 // Create language files
 gulp.task('translate', function () {
-    return gulp.src(src.php)
-    	.pipe(sort())
-        .pipe(wp_pot( {
-            domain: 'conf-schedule',
-            destFile: 'conf-schedule.pot',
-            package: 'Conference_Schedule',
-            bugReport: 'https://github.com/bamadesigner/conference-schedule/issues',
-            lastTranslator: 'Rachel Carden <bamadesigner@gmail.com>',
-            team: 'Rachel Carden <bamadesigner@gmail.com>',
-            headers: false
-        } ))
-        .pipe(gulp.dest('languages/conf-schedule.pot'));
+	return gulp.src(src.php)
+		.pipe(sort())
+		.pipe(wp_pot({
+			domain: 'conf-schedule',
+			destFile: 'conf-schedule.pot',
+			package: 'Conference_Schedule',
+			bugReport: 'https://github.com/bamadesigner/conference-schedule/issues',
+			lastTranslator: 'Rachel Cherry <bamadesigner@gmail.com>',
+			team: 'Rachel Cherry <bamadesigner@gmail.com>',
+			headers: false
+		}))
+		.pipe(gulp.dest(dest.translate));
 });
 
-// Let's get this party started
-gulp.task('default', ['compile','test','translate']);
-gulp.task('compile', ['sass','js']);
-gulp.task('test', ['php']);
+// Test our files.
+gulp.task('test',['php']);
+
+// Compile all the things.
+gulp.task('compile',['sass','js']);
+
+// I've got my eyes on you(r file changes).
+gulp.task('watch',function() {
+	gulp.watch(src.js, ['js']);
+	gulp.watch(src.php, ['test','translate']);
+	gulp.watch(src.sass, ['sass']);
+});
+
+// Let's get this party started.
+gulp.task('default',['compile','test','translate']);
